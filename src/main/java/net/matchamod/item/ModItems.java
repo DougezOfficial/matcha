a48ce5409868dc95678d2b760ac2626a901c180b
DougezOfficial <dougez@outlook.cz>
2025-09-04 19:34:08 +0200

    init

---
+++ src/main/java/net/matchamod/item/ModItems.java
@@ -0,0 +1,10 @@
+package net.matchamod.item;
+
+import net.matchamod.MatchaMod;
+
+public class ModItems {
+
+    public static void registerModItems(){
+        MatchaMod.LOGGER.info("Regtistering Mod Items for " + MatchaMod.MOD_ID);
+    }
+}
