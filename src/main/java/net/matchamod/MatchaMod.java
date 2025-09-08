7cb991775c16383c0b06e26d725b5d5ebb02a96c
DougezOfficial <dougez@outlook.cz>
2025-09-08 14:51:52 +0200

    Added Seeds as the first item

--- src/main/java/net/matchamod/MatchaMod.java
+++ src/main/java/net/matchamod/MatchaMod.java
@@ -2,6 +2,7 @@ package net.matchamod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.matchamod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,5 +12,7 @@ public class MatchaMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-	}
+        ModItems.registerModItems();
+    }
+
 }
--- src/main/java/net/matchamod/item/ModItems.java
+++ src/main/java/net/matchamod/item/ModItems.java
@@ -1,10 +1,22 @@
 package net.matchamod.item;
 
 import net.matchamod.MatchaMod;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+
 
 public class ModItems {
 
-    public static void registerModItems(){
-        MatchaMod.LOGGER.info("Regtistering Mod Items for " + MatchaMod.MOD_ID);
+    public static final Item KRATOM_SEEDS = registerItem("kratom_seeds", new Item(new Item.Settings()));
+
+    private  static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(MatchaMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        MatchaMod.LOGGER.info("Registering Mod Items for " + MatchaMod.MOD_ID);
     }
 }
